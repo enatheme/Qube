cee7b01d5796c544899a57226016a60fc8a5af66
enatheme <enatheme@enatheme.org>
2021-04-25 17:42:55 +0800

    Add auto scrolling, add generation new chunk when needed

--- src/main.py
+++ src/main.py
@@ -58,14 +58,23 @@ class Sky(pygame.sprite.Sprite):
         self.rect.x -= 1
 
 all_sprites = pygame.sprite.Group()
-for i, ii in enumerate(generated):
-    for j, jj in enumerate(reversed(ii)):
+
+def display_column(column_number, column):
+    for j, jj in enumerate(reversed(column)):
         if jj == 1:
-            all_sprites.add(Floor(i, j))
+            all_sprites.add(Floor(column_number, j))
         if jj == 0:
-            all_sprites.add(Sky(i, j))
+            all_sprites.add(Sky(column_number, j))
+
+for i, ii in enumerate(generated):
+    display_column(i, ii)
 
 move = 0
+columns_ready = 10
+generated = quantum.generate_floor_v2(10, generated[9])
+
+display_new_column = False
+automatic_scrolling = False
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -75,20 +84,35 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_d:
                 move = QUBE_UNITY
-                wait = FPS
-
+                display_new_column = True
+            if event.key == pygame.K_u:
+                if automatic_scrolling:
+                    automatic_scrolling = False
+                else:
+                    automatic_scrolling = True
+    if automatic_scrolling:
+        if move == 0:
+            move = QUBE_UNITY
+            display_new_column = True
 
     displaysurface.fill((0,0,0))
 
+    if display_new_column:
+        if move == 0 or move == QUBE_UNITY:
+            display_new_column = False
+            display_column(20, generated[columns_ready - 9])
+            columns_ready -= 1
+            if columns_ready <= 0:
+                columns_ready = 10
+                generated = quantum.generate_floor_v2(10, generated[9])
+                print("generating new columns")
+
+    if move > 0:
+        move -= 1
+        for s in all_sprites:
+            s.move()
+
     for entity in all_sprites:
-        if move > 0:
-            if wait > 0:
-                wait -= 1
-            else:
-                wait = FPS
-                for s in all_sprites:
-                    s.move()
-                move -= 1
         displaysurface.blit(entity.surf, entity.rect)
 
     pygame.display.update()
